9eaf8f095757f908cc4bda72d18333d6bab9bf49
K4ybedenler <kaybedenlerpaul@gmail.com>
2023-01-16 04:09:13 +0400

    deleted trash

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,9 @@
 import dev.inmo.tgbotapi.bot.ktor.telegramBot
-import dev.inmo.tgbotapi.extensions.api.bot.getMe
 import dev.inmo.tgbotapi.extensions.api.chat.get.getChat
-import dev.inmo.tgbotapi.types.chat.ExtendedSupergroupChat
-import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.api.send.sendMessage
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
 import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
-import dev.inmo.tgbotapi.extensions.utils.extendedSupergroupChatOrNull
 import dev.inmo.tgbotapi.extensions.utils.extendedSupergroupChatOrThrow
-import dev.inmo.tgbotapi.requests.send.CopyMessage
-import dev.inmo.tgbotapi.types.chat.Chat
-import dev.inmo.tgbotapi.types.chat.ExtendedChat
-
 
 suspend fun main() {
     haha()
@@ -23,20 +15,15 @@ suspend fun haha() {
     bot.buildBehaviourWithLongPolling {
         onCommand("say") {
 
-            // first try
-            var tryNumOne = getChat(it.chat.id)
-            println(tryNumOne)
-
             // properties of current chat
-            var thisChat = getChat(it.chat).extendedSupergroupChatOrThrow()
+            val thisChat = getChat(it.chat).extendedSupergroupChatOrThrow()
             println("thisChat ---- $thisChat")
 
             // getting id of linked chat
-            var idOfChannelFromGroup = thisChat?.linkedChannelChatId!!
+            val idOfChannelFromGroup = thisChat.linkedChannelChatId!!
             println("idOfChannelFromGroup ---- $idOfChannelFromGroup")
 
             sendMessage(idOfChannelFromGroup, "hahaha")
-//            CopyMessage(idOfChannelFromGroup, idOfCurretChat,)
 
         }
     }.join()
